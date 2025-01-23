df3c93a83ebe52ea3b89f3475980696e0a9249f0
TobyTe141 <tobypainterfox@gmail.com>
2025-01-23 12:52:40 +0000

    test
    I need to get this working in vs code

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'This is the test for dumbasses',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter+= 200;
     });
   }
 
